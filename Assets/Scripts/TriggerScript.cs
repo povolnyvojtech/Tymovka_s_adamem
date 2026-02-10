7b69b2fec4d15a933b5ca72480d475e38be59927
povolny_vojtech <povolny.vojta007@gmail.com>
2026-02-10 17:35:12 +0100

    interaction update

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -1,12 +1,24 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class TriggerScript : MonoBehaviour
 {
+    
+    public TextMeshProUGUI interactionText;
+
+    private void Start()
+    {
+        interactionText.enabled = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Hráč vstoupil do zóny!");
+            interactionText.enabled = true;
+            
         }
     }
     
@@ -15,6 +27,7 @@ public class TriggerScript : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("Hráč opustil zónu.");
+            interactionText.enabled = false;
         }
     }
 }
