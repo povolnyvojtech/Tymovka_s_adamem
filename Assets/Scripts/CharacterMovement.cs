4a4b977616b3e363f57a47d9f77497c418b5fdc1
povolny_vojtech <povolny.vojta007@gmail.com>
2026-02-12 16:42:58 +0100

    blabla bla

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,19 +6,35 @@ public class CharacterMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     private float _moveInput;
+    private bool _facingRight = true;
 
     private void Awake()
     { 
         rb.interpolation = RigidbodyInterpolation2D.Interpolate;
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
     }
+
     private void Update()
     {
         _moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (_moveInput > 0 && !_facingRight || _moveInput < 0 && _facingRight)
+        {
+            Flip();
+        }
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(_moveInput * speed, rb.linearVelocity.y);
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+
+        var currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
     }
 }
