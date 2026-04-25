4a08df4cccf8a71a90e929a158d66b639b11ad71
povolny_vojtech <povolny.vojta007@gmail.com>
2026-04-25 15:26:02 +0200

    photo upload web fix

--- Assets/Scripts/PhotoManager.cs
+++ Assets/Scripts/PhotoManager.cs
@@ -2,18 +2,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using SFB;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System.Runtime.InteropServices;
 
 public class PhotoManager : MonoBehaviour
 {
     private string _path;
-    
     public RawImage[] images;
-    
-    
+
+    // --- WebGL podpora ---
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern void UploadFile(string gameObjectName, string methodName, string filter, bool multiple);
+
+    public void OpenFileExplorer() {
+        // Na WebGL zavoláme JS metodu, která otevře okno prohlížeče
+        // "png, jpg, jpeg" je filtr, "OnWebFileUploaded" je metoda, kterou JS zavolá zpět
+        UploadFile(gameObject.name, "OnWebFileUploaded", ".png, .jpg, .jpeg", false);
+    }
+
+    // Tuto metodu zavolá JavaScript, až uživatel vybere soubor
+    public void OnWebFileUploaded(string url) {
+        _path = url;
+        StartCoroutine(GetPhotos());
+    }
+#else
+    // --- PC / Editor podpora ---
+    public void OpenFileExplorer()
+    {
+        var extensions = new[] {
+            new ExtensionFilter("Image Files", "png", "jpg", "jpeg")
+        };
+
+        StandaloneFileBrowser.OpenFilePanelAsync("Open File", "", extensions, false, (string[] paths) => 
+        {
+            if (paths.Length > 0 && !string.IsNullOrEmpty(paths[0]))
+            {
+                _path = paths[0];
+                if (!_path.StartsWith("file://") && !_path.StartsWith("http"))
+                {
+                    _path = "file://" + _path;
+                }
+                StartCoroutine(GetPhotos());
+            }
+        });
+    }
+#endif
+
     private void Start()
     {
         RefreshPhotos();
@@ -31,30 +68,7 @@ public class PhotoManager : MonoBehaviour
                 continue;
             }
             images[i].enabled = false;
-            
         }
-    }
-
-    public void OpenFileExplorer()
-    {
-        var extensions = new[] {
-            new ExtensionFilter("Image Files", "png", "jpg", "jpeg")
-        };
-
-        StandaloneFileBrowser.OpenFilePanelAsync("Open File", "", extensions, false, (string[] paths) => 
-        {
-            if (paths.Length > 0 && !string.IsNullOrEmpty(paths[0]))
-            {
-                _path = paths[0];
-
-                if (!_path.StartsWith("file://") && !_path.StartsWith("http"))
-                {
-                    _path = "file://" + _path;
-                }
-
-                StartCoroutine(GetPhotos());
-            }
-        });
     }
 
     private IEnumerator GetPhotos()
